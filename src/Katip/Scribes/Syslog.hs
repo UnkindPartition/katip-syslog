305456e8c2ac061b412c9a880e5245342a81d3e2
Roman Cheplyaka <roma@ro-che.info>
2023-03-05 18:20:07 +0200

    Update, optimize, and clean up the code

--- src/Katip/Scribes/Syslog.hs
+++ src/Katip/Scribes/Syslog.hs
@@ -7,58 +7,57 @@ module Katip.Scribes.Syslog
     ( mkSyslogScribe
     ) where
 
-
-import           Control.Exception (SomeException, try)
-import           Control.Monad
-import           Data.Aeson (encode)
-import           Data.ByteString.Lazy (ByteString)
-import           Data.String.Conv
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BSU
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
-import           Katip.Core
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.Text.Lazy.Builder as LTB
+import           Katip
 import           System.Posix.Syslog
+import           Foreign.C.Types
+import           Foreign.ForeignPtr
+import           Foreign.Storable
+import           Foreign.Marshal
 
 --------------------------------------------------------------------------------
 -- | A syslog `Scribe` which respects the main Katip's guidelines.
 -- Returns a tuple containing the `Scribe` and a finaliser.
-mkSyslogScribe :: Namespace -> Severity -> Verbosity -> IO (Scribe, IO ())
+mkSyslogScribe :: Namespace -> Severity -> Verbosity -> IO Scribe
 mkSyslogScribe ns sev verb = do
-  let identifier = T.intercalate "." (unNamespace ns)
-  let cfg = defaultConfig {  identifier   = toS identifier
-                           , options      = [PID, CONS, ODELAY, NDELAY]
-                           , priorityMask = NoMask -- Katip does the masking for us.
-                           }
-#if (MIN_VERSION_katip(0,5,0))
-  let scribe = Scribe (\i@Item{..} -> do
-#if (MIN_VERSION_katip(0,8,0))
-                          permit <- permitItem sev i
-                          when permit $ do
-#else
-                          when (permitItem sev i) $ do
-#endif
-                            res <- try $ withSyslog cfg $ \syslog -> syslog USER (toSyslogPriority _itemSeverity) (toS $ formatItem verb i)
-                            case res of
-                              Left (e :: SomeException) -> putStrLn (show e)
-                              Right () -> return ())
-#if (MIN_VERSION_katip(0,8,0))
-                      (return ()) (permitItem sev)
-#else
-                      (return ())
-#endif
-#else
-  let scribe = Scribe $ \ i@Item{..} -> do
-                            when (permitItem sev i) $ do
-                              res <- try $ withSyslog cfg $ \syslog -> syslog USER (toSyslogPriority _itemSeverity) (toS $ formatItem verb i)
-                              case res of
-                                Left (e :: SomeException) -> putStrLn (show e)
-                                Right () -> return ()
-#endif
-  return (scribe, return ())
+  identifier :: ForeignPtr CChar <- toCString $ T.encodeUtf8 $ T.intercalate "." (unNamespace ns)
+  -- make a copy of identifier that won't be garbage-collected
 
---------------------------------------------------------------------------------
--- | Syslog won't handle correctly things like newlines, so it's programmer's
--- responsibility to escape those.
-formatItem :: LogItem a => Verbosity -> Item a -> ByteString
-formatItem verb = encode . itemJson verb
+  let options = [LogPID, Console]
+
+  withForeignPtr identifier $ \ptr -> openlog ptr options User
+
+  let
+    push i@Item{..} = do
+      let bs = formatItem verb i
+      BSU.unsafeUseAsCStringLen bs $ \cslen ->
+        syslog Nothing (toSyslogPriority _itemSeverity) cslen
+    scribe = Scribe push (closelog >> finalizeForeignPtr identifier) (permitItem sev)
+  return scribe
+
+-- | Create a NULL-terminated 'CString' (wrapped in a 'ForeignPtr') from a
+-- given bytestring.
+toCString :: BS.ByteString -> IO (ForeignPtr CChar)
+toCString bs = BSU.unsafeUseAsCStringLen bs $ \(ptr0, len) -> do
+  fptr <- mallocForeignPtrBytes (len + 1)
+  withForeignPtr fptr $ \ptr -> do
+    copyBytes
+      {- destination -} ptr
+      {- source -}      ptr0
+      {- size -}        len
+    pokeByteOff ptr len (0::CChar)
+  return fptr
+
+formatItem :: LogItem a => Verbosity -> Item a -> BS.ByteString
+formatItem verb = LBS.toStrict . LT.encodeUtf8 . LTB.toLazyText . bracketFormat
+  {- colorize? -} False
+  {- verbosity -} verb
 
 --------------------------------------------------------------------------------
 toSyslogPriority :: Severity -> Priority
